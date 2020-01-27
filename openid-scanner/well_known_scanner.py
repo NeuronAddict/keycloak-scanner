614fa2551d7ec600ac64783c379152a1708dacd0
neuronaddict <dev@lacourarie.org>
2020-01-27 23:07:28 +0100

    Move default realms to config.

--- openid-scanner/well_known_scanner.py
+++ openid-scanner/well_known_scanner.py
@@ -6,26 +6,17 @@ from scan import Scan
 
 URL_PATTERN = '{}/auth/realms/{}/.well-known/openid-configuration'
 
-DEFAULT_REALMS = [
-    'account',
-    'admin-cli',
-    'broker',
-    'realm-management',
-    'security-admin-console'
-
-]
-
 
 class WellKnownScan(Scan):
 
     def perform(self, launch_properties, scan_properties):
         realms = list(scan_properties['realms'].keys())
-        for realm in DEFAULT_REALMS + realms:
+        for realm in realms:
             base_url = launch_properties['base_url']
             url = URL_PATTERN.format(base_url, realm)
             r = requests.get(url)
             if r.status_code != 200:
-                print(colored('[-] Bad status code for realm {} {}: {}'.format(realm, url, r.status_code), 'red'))
+                print(colored('[*] Bad status code for realm {} {}: {}'.format(realm, url, r.status_code), 'gray'))
             else:
-                print(colored('[+] Find a well known for realm {} {}: {}'.format(realm, url, r.status_code), 'green'))
+                print(colored('[+] Find a well known for realm {} {}: {}'.format(realm, url, r.status_code), 'red'))
                 add_kv(scan_properties, 'wellknowns', realm, r.json())
