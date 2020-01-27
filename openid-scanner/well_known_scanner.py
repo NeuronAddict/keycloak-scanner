cceb4761ea57c39ba4b420d4dc6b327b0e70ad72
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-01-27 18:03:15 +0100

    Well known scan.

---
+++ openid-scanner/well_known_scanner.py
@@ -0,0 +1,31 @@
+import requests
+from termcolor import colored
+
+from properties import add_list
+from scan import Scan
+
+URL_PATTERN = '{}/auth/realms/{}/.well-known/openid-configuration'
+
+DEFAULT_REALMS = [
+    'account',
+    'admin-cli',
+    'broker',
+    'realm-management',
+    'security-admin-console'
+
+]
+
+
+class WellKnownScan(Scan):
+
+    def perform(self, launch_properties, scan_properties):
+        realms = launch_properties['realms']
+        for realm in DEFAULT_REALMS + realms:
+            base_url = launch_properties['base_url']
+            url = URL_PATTERN.format(base_url, realm)
+            r = requests.get(url)
+            if r.status_code != 200:
+                print(colored('[-] Bad status code for realm {} {}: {}'.format(realm, url, r.status_code), 'red'))
+            else:
+                print(colored('[+] Find a well known for realm {} {}: {}'.format(realm, url, r.status_code), 'green'))
+                add_list(scan_properties, 'realms', realm)
