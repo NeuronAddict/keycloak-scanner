f3d3f68d87036a45ac645bf3a2c5b09325b85e0a
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-01-27 18:09:24 +0100

    Reformat file

--- openid-scanner/properties.py
+++ openid-scanner/properties.py
@@ -4,6 +4,7 @@ def add_list(scan_properties, key, value):
     else:
         scan_properties[key].append(value)
 
+
 def add_kv(scan_properties, parentkey, key, value):
     if key not in scan_properties or scan_properties[key] is None:
         scan_properties[parentkey] = {key: value}
