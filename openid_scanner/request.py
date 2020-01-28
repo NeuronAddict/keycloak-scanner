7807d2959146312955a743b0dfa185954e3ad510
neuronaddict <dev@lacourarie.org>
2020-01-28 15:43:18 +0100

    None signature scan.

--- openid_scanner/request.py
+++ openid_scanner/request.py
@@ -3,10 +3,12 @@ from requests import Session
 
 class Request:
     proxy = {}
+    verify = True
 
     @staticmethod
     def request():
         session = Session()
         session.headers = {'User-Agent': 'Keycloak scanner'}
         session.proxies = Request.proxy
+        session.verify = Request.verify
         return session
