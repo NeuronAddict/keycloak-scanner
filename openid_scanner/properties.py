cfa17551a763de51d63066d65b5a61b5ae30a11e
neuronaddict <dev@lacourarie.org>
2020-01-28 10:52:33 +0100

    Fix key value property add

--- openid_scanner/properties.py
+++ openid_scanner/properties.py
@@ -5,8 +5,8 @@ def add_list(scan_properties, key, value):
         scan_properties[key].append(value)
 
 
-def add_kv(scan_properties, parentkey, key, value):
-    if parentkey not in scan_properties or scan_properties[parentkey] is None:
-        scan_properties[parentkey] = [{key: value}]
+def add_kv(scan_properties, parent_key, key, value):
+    if parent_key not in scan_properties or scan_properties[parent_key] is None:
+        scan_properties[parent_key] = {key: value}
     else:
-        scan_properties[parentkey].append({key: value})
+        scan_properties[parent_key][key] = value
