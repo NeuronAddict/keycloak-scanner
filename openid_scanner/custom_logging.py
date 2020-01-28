30130cc1b98ecec2efb6193fe2dca908e74fc319
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2020-01-28 17:28:40 +0100

    Make vulnerabilities more visibles.

--- openid_scanner/custom_logging.py
+++ openid_scanner/custom_logging.py
@@ -2,15 +2,16 @@ from termcolor import colored
 
 verbose_mode = False
 
+
 def info(msg):
-    print('[*] {}'.format(msg))
+    print('[INFO] {}'.format(msg))
 
 
 def verbose(msg, color='gray'):
     if verbose_mode:
-        print(colored('[*] {}'.format(msg), color))
+        print(colored('[VERBOSE] {}'.format(msg), color))
 
 
 def find(msg):
-    print(colored('[+] {}'.format(msg), color='green'))
+    print(colored('[VULNERABILITY] {}'.format(msg), color='red'))
 
