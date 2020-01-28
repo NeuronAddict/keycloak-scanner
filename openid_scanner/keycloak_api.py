0324af571a3ec9e95646287e2c099d85a27e4c28
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2020-01-28 15:43:18 +0100

    None signature scan.

---
+++ openid_scanner/keycloak_api.py
@@ -0,0 +1,27 @@
+from request import Request
+
+
+class KeyCloakApi:
+
+    def __init__(self, well_known):
+        self.well_known = well_known
+        
+    def get_token(self, client_id, client_secret, username, password):
+        r = Request.request().post(self.well_known['token_endpoint'],
+                                  data={
+                                      'client_id': client_id,
+                                      'username': username,
+                                      'password': password,
+                                      'grant_type': 'password',
+                                      'client_secret': client_secret
+                                  })
+        r.raise_for_status()
+        res = r.json()
+        return res['access_token'], res['refresh_token']
+
+    def refresh(self, client, refresh_token):
+        data = {'refresh_token': refresh_token, 'grant_type': 'refresh_token', 'client_id': client}
+        r = Request.request().post(self.well_known['token_endpoint'], data=data)
+        r.raise_for_status()
+        res = r.json()
+        return res['access_token'], res['refresh_token']
