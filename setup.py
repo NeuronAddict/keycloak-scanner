2e2d8166f3cbd8f2d5daf5227f7e5263457fd0b6
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2020-01-29 23:11:20 +0100

    Executable in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(name='keycloak-scanner',
       url='http://github.com/neuronaddict/keycloak-scanner',
       author='neuronaddict',
       author_email='',
+      entry_points={
+          'console_scripts': [
+              'keycloak-scanner = keycloak_scanner.main:main'
+          ]
+      },
       license='GNUv3',
       packages=['keycloak_scanner'],
       zip_safe=False, install_requires=['requests', 'termcolor', 'pyjwt'])
