92d793fa4bc697a52b99a66237165b7d8da99f8e
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-01-29 16:22:05 +0100

    Github url in user-agent.

--- keycloak_scanner/request.py
+++ keycloak_scanner/request.py
@@ -8,7 +8,7 @@ class Request:
     @staticmethod
     def request():
         session = Session()
-        session.headers = {'User-Agent': 'Keycloak scanner'}
+        session.headers = {'User-Agent': 'Keycloak scanner - https://github.com/NeuronAddict/keycloak-scanner'}
         session.proxies = Request.proxy
         session.verify = Request.verify
         return session
