ce092a207fc8f984e03d32c9194bc64527bc4ebf
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-03-05 16:02:26 +0100

    Display scan configuration and datetime when start scan.

--- keycloak_scanner/scanner.py
+++ keycloak_scanner/scanner.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
+from custom_logging import info
 from keycloak_scanner.clients_scanner import ClientScan
 from keycloak_scanner.form_post_xss_scan import FormPostXssScan
 from keycloak_scanner.none_sign_scan import NoneSignScan
 from keycloak_scanner.open_redirect_scanner import OpenRedirectScan
 from keycloak_scanner.realm_scanner import RealmScanner
-from keycloak_scanner.well_known_scanner import WellKnownScan
 from keycloak_scanner.security_console_scanner import SecurityConsoleScan
+from keycloak_scanner.well_known_scanner import WellKnownScan
 
 SCANS = [
     RealmScanner(),
@@ -19,7 +22,7 @@ SCANS = [
 
 class Scanner:
 
-    def __init__(self, launch_config, scans=None):
+    def __init__(self, launch_config: dict, scans=None):
         if scans is None:
             scans = SCANS
         self.launch_config = launch_config
@@ -31,5 +34,8 @@ class Scanner:
             scan.init(self.launch_config, self.scan_properties)
 
     def start(self):
+        ofsuscated_config = self.launch_config.copy()
+        ofsuscated_config['password'] = '*' * 4
+        info('Start scan at {}.\nOptions {}'.format(datetime.now(), ofsuscated_config))
         for scan in self.scans:
             scan.perform(self.launch_config, self.scan_properties)
