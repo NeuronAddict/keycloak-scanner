65f2644e540f38c348fec0a48c1f58e97adf6dad
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-21 22:26:01 +0200

    reanme scanner as MasterScanner

--- keycloak_scanner/main.py
+++ keycloak_scanner/main.py
@@ -11,7 +11,7 @@ from keycloak_scanner.form_post_xss_scan import FormPostXssScan
 from keycloak_scanner.none_sign_scan import NoneSignScan
 from keycloak_scanner.open_redirect_scanner import OpenRedirectScan
 from keycloak_scanner.realm_scanner import RealmScanner
-from keycloak_scanner.scanner import Scanner
+from keycloak_scanner.masterscanner import MasterScanner
 from keycloak_scanner.security_console_scanner import SecurityConsoleScan
 from keycloak_scanner.well_known_scanner import WellKnownScan
 
@@ -83,7 +83,7 @@ def start(args, session: requests.Session):
         NoneSignScan()
     ]
 
-    scanner = Scanner({
+    scanner = MasterScanner({
         'base_url': args.base_url,
         'realms': realms,
         'clients': clients,
