aa443d79282e5c258f4bd1e56beec4dc6bafb706
neuronaddict <manufrommars@elacourarie.org>
2021-05-21 23:53:32 +0200

    refactor client scanner.

---
+++ tests/mock_response.py
@@ -0,0 +1,15 @@
+class MockResponse:
+
+    def __init__(self, status_code, response):
+        self.status_code = status_code
+        self.response = response
+        self.text = response
+
+    def json(self):
+        if isinstance(self.response, dict):
+            return {'response_types_supported': ['code'],
+                    'authorization_endpoint': 'http://testscan/auth',
+                    'response_modes_supported': ['form_post']
+                    }
+        else:
+            raise Exception(f'response spec is not json ({self.response})')
