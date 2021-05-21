f6f019cf77a8033361ac048c20e4cdd001935a35
neuronaddict <manufrommars@elacourarie.org>
2021-05-22 01:40:11 +0200

    Mock response for full_scan

--- tests/mock_response.py
+++ tests/mock_response.py
@@ -5,11 +5,12 @@ class MockResponse:
         self.response = response
         self.text = response
 
+    def raise_for_status(self):
+        if self.status_code > 399:
+            raise Exception('Mock raise for status')
+
     def json(self):
         if isinstance(self.response, dict):
-            return {'response_types_supported': ['code'],
-                    'authorization_endpoint': 'http://testscan/auth',
-                    'response_modes_supported': ['form_post']
-                    }
+            return self.response
         else:
             raise Exception(f'response spec is not json ({self.response})')
