492a0ef08edd1efbf42a24ad64b5285d53eaa58e
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-23 18:11:08 +0200

    provide Realms and WellKnown in conftest

--- tests/test_realm_scanner.py
+++ tests/test_realm_scanner.py
@@ -3,13 +3,10 @@ from requests import Session
 from keycloak_scanner.scanners.realm_scanner import RealmScanner, Realm
 
 
-def test_perform(full_scan_mock_session: Session, master_realm: dict, other_realm: dict):
+def test_perform(full_scan_mock_session: Session, master_realm: Realm, other_realm: Realm):
 
     scanner = RealmScanner(base_url='http://testscan', realms=['master', 'other'], session=full_scan_mock_session)
 
     result = scanner.perform()
 
-    assert result == [
-        Realm('master', 'http://testscan/auth/realms/master', master_realm),
-        Realm('other', 'http://testscan/auth/realms/other', other_realm)
-    ]
+    assert result == [master_realm, other_realm]
