ee344c234429cba539970466c0f0a1cefe5ccae1
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-23 18:44:32 +0200

    dependency support for client scanner

--- tests/test_clients_scanner.py
+++ tests/test_clients_scanner.py
@@ -1,24 +1,23 @@
 from unittest.mock import MagicMock
 
 import requests
+from requests import Session
 
-from keycloak_scanner.scanners.clients_scanner import ClientScanner
+from keycloak_scanner.scanners.clients_scanner import ClientScanner, Client
+from keycloak_scanner.scanners.realm_scanner import Realm
+from keycloak_scanner.scanners.well_known_scanner import WellKnownDict
 from tests.mock_response import MockResponse
 
 
-def test_perform():
+def test_perform(base_url: str, full_scan_mock_session: Session, master_realm: Realm, other_realm: Realm,
+                 well_known_dict: WellKnownDict, client1: Client, client2: Client):
 
-    session = requests.Session()
-    session.get = MagicMock(return_value=MockResponse(status_code=200, response={}))
+    client_scanner = ClientScanner(clients=['client1', 'client2'], base_url='http://localhost:8080', session=full_scan_mock_session)
 
-    client_scanner = ClientScanner(clients=[], base_url='http://test', session=session)
+    realms = [master_realm]
 
-    scan_properties = {
-        'realms': {
-            'master': '',
-            'other': ''
-        }
-    }
-    client_scanner.perform(scan_properties)
+    result = client_scanner.perform(realms=realms, well_known_dict=well_known_dict)
 
-    assert scan_properties == {'clients': {}, 'realms': {'master': '', 'other': ''}}
+    assert result == [
+        client1, client2
+    ]
