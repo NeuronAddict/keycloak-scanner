4dcb676a73364b84ccb102b612fcb2d48a827bf4
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-24 00:05:02 +0200

    Fix well known test

--- tests/test_well_known_scanner.py
+++ tests/test_well_known_scanner.py
@@ -1,13 +1,13 @@
 from requests import Session
 
-from keycloak_scanner.scanners.realm_scanner import Realm
-from keycloak_scanner.scanners.well_known_scanner import WellKnownScanner, WellKnown, WellKnownDict
+from keycloak_scanner.scanners.realm_scanner import Realms
+from keycloak_scanner.scanners.well_known_scanner import WellKnownScanner, WellKnownDict
 
 
-def test_perform(base_url: str, master_realm: Realm, other_realm: Realm, full_scan_mock_session: Session, well_known_dict: WellKnownDict):
+def test_perform(base_url: str, all_realms: Realms, full_scan_mock_session: Session, well_known_dict: WellKnownDict):
 
     scanner = WellKnownScanner(base_url=base_url, session=full_scan_mock_session)
 
-    result = scanner.perform([master_realm, other_realm])
+    result = scanner.perform(all_realms)
 
     assert result == well_known_dict
