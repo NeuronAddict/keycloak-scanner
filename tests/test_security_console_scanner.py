09627a5f51a624d4f22ea1630237f5a27c057d9f
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-23 22:08:00 +0200

    test security console scanner

---
+++ tests/test_security_console_scanner.py
@@ -0,0 +1,14 @@
+from requests import Session
+
+from keycloak_scanner.scanners.realm_scanner import Realm, Realms
+from keycloak_scanner.scanners.security_console_scanner import SecurityConsoleScanner, SecurityConsoleResults
+
+
+def test_perform(master_realm: Realm, base_url: str, full_scan_mock_session: Session):
+
+    scanner = SecurityConsoleScanner(base_url=base_url, session=full_scan_mock_session)
+    result = scanner.perform(Realms([master_realm]))
+
+    assert result == SecurityConsoleResults(
+
+    )
