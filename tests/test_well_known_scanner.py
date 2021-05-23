a75c68e78d510bd213361f3d2f1b234700f63321
neuronaddict <manufrommars@elacourarie.org>
2021-05-23 17:36:35 +0200

    wellknown as dict

--- tests/test_well_known_scanner.py
+++ tests/test_well_known_scanner.py
@@ -12,6 +12,6 @@ def test_perform(full_scan_mock_session: Session, master_realm: dict, well_known
 
     result = scanner.perform([master_realm])
 
-    assert result == [
-        WellKnown(master_realm, name='master', url='http://testscan/auth/realms/master/.well-known/openid-configuration', json=well_known)
-    ]
+    assert result == {
+        'master': WellKnown(master_realm, name='master', url='http://testscan/auth/realms/master/.well-known/openid-configuration', json=well_known)
+    }
