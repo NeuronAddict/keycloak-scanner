28ff8463ac65c734376f9f867025007b869c72b4
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-23 17:33:46 +0200

    dependency support for WellKnownScanner

--- keycloak_scanner/scanners/well_known_scanner.py
+++ keycloak_scanner/scanners/well_known_scanner.py
@@ -1,22 +1,43 @@
+from typing import List
+
 from keycloak_scanner.properties import add_kv
+from keycloak_scanner.scanners.json_result import JsonResult
+from keycloak_scanner.scanners.realm_scanner import Realm, Realms
 from keycloak_scanner.scanners.scanner import Scanner
+from keycloak_scanner.scanners.scanner_pieces import Need
 
 URL_PATTERN = '{}/auth/realms/{}/.well-known/openid-configuration'
 
 
-class WellKnownScanner(Scanner):
+class WellKnown(JsonResult):
+
+    def __init__(self, realm: Realm, **kwargs):
+        self.realm = realm
+        super().__init__(**kwargs)
+
+
+WellKnownList = List[WellKnown]
+
+
+class WellKnownScanner(Need[Realms], Scanner):
 
     def __init__(self, **kwars):
         super().__init__(**kwars)
 
-    def perform(self, scan_properties):
-        realms = scan_properties['realms'].keys()
+    def perform(self, realms: Realms, **kwargs):
+
+        result: WellKnownList = []
+
         for realm in realms:
 
-            url = URL_PATTERN.format(super().base_url(), realm)
+            url = URL_PATTERN.format(super().base_url(), realm.name)
             r = super().session().get(url)
+
             if r.status_code != 200:
                 super().verbose('Bad status code for realm {} {}: {}'.format(realm, url, r.status_code))
+
             else:
                 super().info('Find a well known for realm {} {}'.format(realm, url))
-                add_kv(scan_properties, 'wellknowns', realm, r.json())
+                result.append(WellKnown(realm, name=realm.name, url=url, json=r.json()))
+
+        return result
