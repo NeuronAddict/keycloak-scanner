9f0519069dc4385bf52c2d21a595e3e35cc8445f
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-23 20:43:01 +0200

    Base url in client scanner test

--- tests/test_clients_scanner.py
+++ tests/test_clients_scanner.py
@@ -12,7 +12,7 @@ from tests.mock_response import MockResponse
 def test_perform(base_url: str, full_scan_mock_session: Session, master_realm: Realm, other_realm: Realm,
                  well_known_dict: WellKnownDict, client1: Client, client2: Client):
 
-    client_scanner = ClientScanner(clients=['client1', 'client2'], base_url='http://localhost:8080', session=full_scan_mock_session)
+    client_scanner = ClientScanner(clients=['client1', 'client2'], base_url=base_url, session=full_scan_mock_session)
 
     realms = [master_realm]
 
