5ad9b252fccf2761f680ddaf1a6bae1dddc1c81e
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-24 04:01:14 +0200

    provide version in python file.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import io
+import re
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+__version__ = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+          io.open('keycloak_scanner/_version.py', encoding='utf_8_sig').read()
+          ).group(1)
+
 setup(name='keycloak-scanner',
-      version='1.0.3beta',
+      version=__version__,
       description='Keycloak vulnerabilities scanner',
       url='http://github.com/neuronaddict/keycloak-scanner',
       author='neuronaddict',
@@ -20,8 +27,8 @@ setup(name='keycloak-scanner',
       packages=find_packages(),
       zip_safe=False, install_requires=['requests', 'termcolor', 'pyjwt', 'urllib3'],
       classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
       ],
       python_requires='>=3.6')
