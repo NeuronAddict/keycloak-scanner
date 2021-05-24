e468da3f33301923f8083a0dd0a832823ba8c8e9
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-24 02:03:59 +0200

    UNSTABLE covariant tuple as scan result

--- keycloak_scanner/scanners/scanner.py
+++ keycloak_scanner/scanners/scanner.py
@@ -1,12 +1,13 @@
 from typing import TypeVar, Generic
 
 from keycloak_scanner.logging.printlogger import PrintLogger
+from keycloak_scanner.logging.vuln_flag import VulnFlag
 from keycloak_scanner.scanners.session_holder import SessionHolder
 
-T = TypeVar('T')
+Tco = TypeVar('Tco', covariant=True)
 
 
-class Scanner(Generic[T], SessionHolder, PrintLogger):
+class Scanner(Generic[Tco], SessionHolder, PrintLogger):
 
     def __init__(self, base_url: str, **kwargs):
         self.base_url_ = base_url
@@ -24,8 +25,7 @@ class Scanner(Generic[T], SessionHolder, PrintLogger):
         super().info(f'Start logger {self.name()}')
         assert not hasattr(super(), 'init_scan')
 
-
-    def perform(self, **kwargs) -> T:
+    def perform(self, **kwargs) -> (Tco, VulnFlag):
         """
         Perform the scan
         :return: scan result (json)
