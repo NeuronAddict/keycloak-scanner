ae88a5f7903439e0dd5ee6f8722aaa0231b8135c
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-24 17:48:16 +0200

    get supported grant types in well known

--- tests/test_well_known_scanner.py
+++ tests/test_well_known_scanner.py
@@ -12,4 +12,12 @@ def test_perform(base_url: str, all_realms: Realms, full_scan_mock_session: Sess
 
     assert result == well_known_dict
 
+    assert well_known_dict['master'].allowed_grants() == ['authorization_code',
+                                                          'implicit',
+                                                          'refresh_token',
+                                                          'password',
+                                                          'client_credentials',
+                                                          'urn:ietf:params:oauth:grant-type:device_code',
+                                                          'urn:openid:params:grant-type:ciba']
+
     assert not vf.has_vuln
