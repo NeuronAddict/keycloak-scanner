b19c140b07b6ae542bd835bde198c994a00134f0
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-25 23:16:30 +0200

    keycloak api test + new mock response api

--- tests/mock_response.py
+++ tests/mock_response.py
@@ -1,9 +1,16 @@
+from typing import Dict, Callable
+from unittest.mock import MagicMock
+
+import requests
+
 from keycloak_scanner.logging.printlogger import PrintLogger
 
 
 class MockResponse:
 
-    def __init__(self, status_code, response=None, headers={}):
+    def __init__(self, status_code, response=None, headers=None):
+        if headers is None:
+            headers = {}
         self.status_code = status_code
         self.response = response
         self.text = response
@@ -39,3 +46,38 @@ class MockPrintLogger(PrintLogger):
     def warn(self, message: str):
         self.warns.append(message)
         super().warn(message)
+
+
+class RequestSpec:
+
+    def __init__(self, response: MockResponse, assertion: Callable[..., bool] = (lambda **kwargs: True)):
+        self.assertion = assertion
+        self.response = response
+
+
+def mock_session(get=None, post=None) -> requests.session():
+
+    if post is None:
+        post = {}
+    if get is None:
+        get = {}
+
+    def get_mock_response(url, **kwargs):
+
+        if url not in get:
+            raise Exception(f'[make_mock_session] Bad url test (GET) : {url}')
+        assert get[url].assertion(**kwargs)
+        return get[url].response
+
+    def post_mock_response(url, **kwargs):
+
+        if url not in post:
+            raise Exception(f'[make_mock_session] Bad url test (POST) : {url}')
+        assert post[url].assertion(**kwargs)
+        return post[url].response
+
+    session = requests.Session()
+    session.get = MagicMock(side_effect=get_mock_response)
+    session.post = MagicMock(side_effect=post_mock_response)
+
+    return session
