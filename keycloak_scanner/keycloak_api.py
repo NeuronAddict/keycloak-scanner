f2302dfdd43c3f5d34c164447140289ab3f5f896
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-25 02:11:30 +0200

    Try fix test login scanner

--- keycloak_scanner/keycloak_api.py
+++ keycloak_scanner/keycloak_api.py
@@ -71,7 +71,7 @@ class KeyCloakApi(PrintLogger, SessionHolder):
                         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                         "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
                         "Sec-Fetch-Dest": "document",
-                        "Referer": r.request.url,
+                        "Referer": url,
                         "Accept-Encoding": "gzip, deflate", "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7"}
 
         auth_data = {"username": username, "password": password}
