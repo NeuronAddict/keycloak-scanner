5460f714f8be7828e224db6d227fa3da1eb05e51
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-25 21:38:12 +0200

    remove unused fixture

--- itests/conftest.py
+++ itests/conftest.py
@@ -10,7 +10,3 @@ from keycloak_scanner.logging.http_logging import httpclient_logging_patch
 def base_url():
     return 'http://localhost:8080'
 
-
-@fixture
-def session() -> requests.Session:
-    return requests.session()
