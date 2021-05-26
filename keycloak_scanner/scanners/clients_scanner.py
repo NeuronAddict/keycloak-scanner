746ab4310dab1c03b2fd68daef3011483b69c270
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-27 00:57:11 +0200

    Try fix client scanner

--- keycloak_scanner/scanners/clients_scanner.py
+++ keycloak_scanner/scanners/clients_scanner.py
@@ -1,12 +1,12 @@
 from typing import List
 
 from keycloak_scanner.logging.vuln_flag import VulnFlag
-from keycloak_scanner.scanners.realm_scanner import Realms
+from keycloak_scanner.scanners.realm_scanner import Realms, Realm
 from keycloak_scanner.scanners.scanner import Scanner
 from keycloak_scanner.scanners.scanner_pieces import Need2
 from keycloak_scanner.scanners.well_known_scanner import WellKnownDict
 
-URL_PATTERN = '{}/auth/realms/{}/{}'
+
 
 
 class Client:
@@ -35,37 +35,50 @@ class ClientScanner(Need2[Realms, WellKnownDict], Scanner[Clients]):
         self.clients = clients
         super().__init__(**kwargs)
 
+    def has_endpoint(self, realm: Realm, client_name: str) -> str:
+        url = f'{super().base_url()}/auth/realms/{realm.name}/{client_name}'
+
+        try:
+            r = super().session().get(url)
+            r.raise_for_status()
+            super().info('Find a client for realm {}: {}'.format(realm.name, client_name))
+            return url
+        except Exception as e:
+            super().info(f'[ClientScanner]: {e}')
+
+
     def perform(self, realms: Realms, well_known_dict: WellKnownDict, **kwargs) -> (Clients, VulnFlag):
 
         result: Clients = Clients()
 
         for realm in realms:
             for client_name in self.clients:
-                url = URL_PATTERN.format(super().base_url(), realm.name, client_name)
 
-                try:
-                    r = super().session().get(url)
-                    r.raise_for_status()
+                url = self.has_endpoint(realm, client_name)
 
-                except Exception as e:
-                    super().info(f'[ClientScanner]: {e}')
-                    url = None
+                # TODO: auth endpoint in other scanner ?
+                auth_url = self.has_auth_endpoint(client_name, realm, well_known_dict)
 
-                try:
-
-                    auth_url = well_known_dict[realm.name].json['authorization_endpoint']
-
-                    r = super().session().get(auth_url, params={'client_id': client_name}, allow_redirects=False)
-                    if r.status_code == 302:
-                        super().info('Find a client for realm {}: {}'.format(realm.name, client_name))
-                        result.append(Client(name=client_name, url=url, auth_endpoint=auth_url))
-                    else:
-                        super().verbose('client {} seems to not exists'.format(client_name))
-
-                except KeyError as e:
-                    print(f'realm {realm.name}\'s wellknown doesn\t existsor do not have "authorization_endpoint". ({well_known_dict})')
-                    auth_url = None
-
-                result.append(Client(name=client_name, auth_endpoint=auth_url, url=url))
+                if auth_url is not None or url is not None:
+                    result.append(Client(name=client_name, auth_endpoint=auth_url, url=url))
 
         return result, VulnFlag(False)
+
+    def has_auth_endpoint(self, client_name, realm, well_known_dict) -> str:
+        try:
+
+            auth_url = well_known_dict[realm.name].json['authorization_endpoint']
+
+            r = super().session().get(auth_url, params={'client_id': client_name}, allow_redirects=False)
+            # TODO : is code 400 always an existing client ?
+            if r.status_code == 302 or r.status_code == 400:
+                super().info('Find a client auth endpoint for realm {}: {}'.format(realm.name, client_name))
+            else:
+                auth_url = None
+
+        except KeyError as e:
+            print(
+                f'realm {realm.name}\'s wellknown doesn\'t exists or do not have "authorization_endpoint". ({well_known_dict})')
+            print(e)
+            auth_url = None
+        return auth_url
