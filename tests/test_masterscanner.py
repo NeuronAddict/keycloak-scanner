846015af3b63c52f5d1e4e56c4c3a99395f206c2
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-27 22:51:38 +0200

    Fail fast mode

--- tests/test_masterscanner.py
+++ tests/test_masterscanner.py
@@ -1,9 +1,11 @@
 from unittest.mock import MagicMock
 
+import pytest
 import requests
 from _pytest.capture import CaptureFixture
 from requests import Session
 
+from keycloak_scanner.logging.vuln_flag import VulnFlag
 from keycloak_scanner.masterscanner import MasterScanner, to_camel_case
 from keycloak_scanner.scanners.clients_scanner import ClientScanner, Client, Clients
 from keycloak_scanner.scanners.form_post_xss_scanner import FormPostXssScanner
@@ -11,6 +13,7 @@ from keycloak_scanner.scanners.login_scanner import LoginScanner
 from keycloak_scanner.scanners.none_sign_scanner import NoneSignScanner
 from keycloak_scanner.scanners.open_redirect_scanner import OpenRedirectScanner
 from keycloak_scanner.scanners.realm_scanner import RealmScanner, Realm, Realms
+from keycloak_scanner.scanners.scanner import Scanner
 from keycloak_scanner.scanners.security_console_scanner import SecurityConsoleScanner
 from keycloak_scanner.scanners.well_known_scanner import WellKnownScanner
 from tests.mock_response import MockResponse, MockSpec, RequestSpec
@@ -106,5 +109,63 @@ def test_camel_case():
     assert to_camel_case('WellKnown') == 'well_known'
     assert to_camel_case('Realms') == 'realms'
 
-def test_should_fail_fast():
-    pass
+
+
+
+
+def test_should_fail_fast(base_url: str, full_scan_mock: MockSpec, capsys: CaptureFixture):
+
+    common_args = {
+        'base_url': base_url,
+        'session_provider': lambda: full_scan_mock.session()
+    }
+
+    class FailFastException(Exception):
+        pass
+
+    class ErrorScanner(Scanner[str]):
+
+        def perform(self, **kwargs) -> (str, VulnFlag):
+            raise FailFastException()
+
+    ms = MasterScanner(scans=[
+        RealmScanner(**common_args, realms=['master', 'other']),
+        WellKnownScanner(**common_args),
+        ErrorScanner(**common_args),
+        ClientScanner(**common_args, clients=['client1', 'client2']),
+        LoginScanner(**common_args, username='admin', password='admin'),
+        SecurityConsoleScanner(**common_args),
+        OpenRedirectScanner(**common_args),
+        FormPostXssScanner(**common_args),
+        NoneSignScanner(**common_args)
+    ])
+
+    with pytest.raises(FailFastException) as e:
+
+        status = ms.start()
+
+
+    captured = capsys.readouterr()
+
+    print(captured.out)
+
+    assert captured.err == ''
+
+    assert 'Find realm master' in captured.out
+
+    assert 'Public key for realm master : ' in captured.out
+
+    assert "[INFO] Find a well known for realm Realm('master'" in captured.out
+
+    assert "[INFO] Find a client for realm master: client1" not in captured.out
+
+    assert "[INFO] Find a client for realm master: client2" not in captured.out
+
+    assert "[INFO] Find a client for realm other: client1" not in captured.out
+
+    assert "[INFO] Find a client for realm other: client2" not in captured.out
+
+    # TODO : all login work in mock
+    assert "[+] LoginScanner - Form login work for admin on realm other, client client1, (<openid location>)" not in captured.out
+
+    assert "[+] LoginScanner - Can login with username admin on realm other, client client2, grant_type: password" not in captured.out
