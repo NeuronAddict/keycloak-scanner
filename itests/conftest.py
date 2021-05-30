4de4a9d2e827ffce50c260462fe0e7e10922a9b9
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-05-30 14:36:17 +0200

    Fix tests for client registration

--- itests/conftest.py
+++ itests/conftest.py
@@ -1,4 +1,5 @@
-import requests
+import os
+
 from _pytest.fixtures import fixture
 
 from keycloak_scanner.logging.http_logging import httpclient_logging_patch
@@ -6,7 +7,13 @@ from keycloak_scanner.logging.http_logging import httpclient_logging_patch
 # use it for debug, but this make start_test fail
 # httpclient_logging_patch()
 
+
 @fixture
-def base_url():
+def base_url() -> str:
     return 'http://localhost:8080'
 
+
+@fixture
+def proxy() -> str:
+    if os.getenv('PROXY'):
+        return os.getenv('PROXY')
