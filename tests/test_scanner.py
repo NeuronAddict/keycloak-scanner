21316fdf3be8241aee8762b27d3de43b0b263c0b
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-06-13 14:56:19 +0200

    scanner args with named args

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -12,8 +12,10 @@ def test_status():
     status.args('truc', 'test')
     status.args('truc', 'test2')
 
-    assert list(status.args('bidule', True)) == [(2, 'test', True), (2, 'test2', True), (4, 'test', True),
-                                                 (4, 'test2', True)]
+    assert list(status.args('bidule', True)) == [{'machin': 2, 'truc': 'test', 'bidule': True},
+                                                 {'machin': 2, 'truc': 'test2', 'bidule': True},
+                                                 {'machin': 4, 'truc': 'test', 'bidule': True},
+                                                 {'machin': 4, 'truc': 'test2', 'bidule': True}]
 
 
 def test_should_fail_when_add_too_many_args():
