2ae9c5c0a2bf916c92322b84bc6b2d8878316b58
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-06-14 00:31:15 +0200

    fix realm type test

--- tests/test_realm_scanner.py
+++ tests/test_realm_scanner.py
@@ -2,18 +2,19 @@ from requests import Session
 
 from keycloak_scanner.scanners.mediator import Mediator
 from keycloak_scanner.scanners.realm_scanner import RealmScanner
-from keycloak_scanner.scanners.types import Realm, RealmType
+from keycloak_scanner.scanners.types import Realm, realmType
 
 
-def test_perform(base_url: str, full_scan_mock_session: Session, master_realm: Realm, other_realm: Realm):
+def test_perform_with_event(base_url: str, full_scan_mock_session: Session, master_realm: Realm, other_realm: Realm):
 
     mediator = Mediator()
 
-    scanner = RealmScanner(mediator=mediator, result_type=RealmType, base_url=base_url, realms=['master', 'other'],
+    scanner = RealmScanner(mediator=mediator, result_type=realmType, base_url=base_url, realms=['master', 'other'],
                            session_provider=lambda: full_scan_mock_session)
 
     scanner.perform_base()
 
-    assert mediator.scan_results.results['realm'] == [master_realm, other_realm]
+    assert mediator.scan_results.get(realmType) == [master_realm, other_realm]
 
-    assert not vf.has_vuln
+    # TODO: vuln flag ?
+    #assert not vf.has_vuln
