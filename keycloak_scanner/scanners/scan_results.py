44bb569abddea2b4697a1b54256251e947322cb5
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-06-13 02:39:54 +0200

    Scanner with types

--- keycloak_scanner/scanners/scan_results.py
+++ keycloak_scanner/scanners/scan_results.py
@@ -1,24 +1,25 @@
-from typing import Dict, Any
+from typing import Dict, Any, TypeVar
 
 from keycloak_scanner.logging.printlogger import PrintLogger
 from keycloak_scanner.scanners.scanner_exceptions import DuplicateResultException
 from keycloak_scanner.utils import to_camel_case
 
+T = TypeVar('T')
+
 
 class ScanResults(PrintLogger):
 
-    def __init__(self, previous_deps: Dict[str, Any], **kwargs):
+    def __init__(self, previous_deps: Dict[str, Any] = None, **kwargs):
         if previous_deps is None:
             previous_deps = {}
         self.results: Dict[str, Any] = previous_deps
         super().__init__(**kwargs)
 
-    def add(self, result: Any):
-        key = to_camel_case(result.__class__.__name__)
-        if key in self.results:
+    def add(self, name, result: T):
+        if name in self.results:
             raise DuplicateResultException(result)
-        super().verbose(f'new result with key: {key} ({result})')
-        self.results[key] = result
+        super().verbose(f'new result with key: {name} ({result})')
+        self.results[name] = result
 
     def __repr__(self):
         return repr(self.results)
