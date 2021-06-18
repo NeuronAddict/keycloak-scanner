f8d7716ec27342c5a8df400e398b2f9d3278c676
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-06-19 00:42:04 +0200

    check type in wrapper

--- keycloak_scanner/scanners/wrap.py
+++ keycloak_scanner/scanners/wrap.py
@@ -9,17 +9,30 @@ SimpleType = TypeVar('SimpleType')
 V = TypeVar('V')
 
 
+class BadWrappedTypeException(Exception):
+
+    def __init__(self, t: type, value):
+        self.t = t
+        self.value = value
+        super().__init__(f'Wrapper error: value {value} not compatible with type {t.__name__}')
+
+
 class WrapperType(Generic[SimpleType]):
 
     def __init__(self, simple_type: type):
         self.name = to_camel_case(simple_type.__name__)
         self.simple_type = simple_type
 
+    def check(self, value):
+        if not isinstance(value, self.simple_type):
+            raise BadWrappedTypeException(self.simple_type, value)
+
 
 class Wrapper(Generic[SimpleType]):
 
     def __init__(self, wrapper_type: WrapperType[SimpleType], value: SimpleType):
         self.wrapper_type: WrapperType[SimpleType] = wrapper_type
+        self.wrapper_type.check(value)
         self.value_ = value
 
     def value(self) -> SimpleType:
