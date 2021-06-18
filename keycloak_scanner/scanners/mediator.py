d72c02beea2fe933da36606402c6a379e825392e
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-06-18 23:50:24 +0200

    use raw types for identify results

--- keycloak_scanner/scanners/mediator.py
+++ keycloak_scanner/scanners/mediator.py
@@ -2,6 +2,7 @@ from typing import TypeVar, List, Dict, Any
 
 from keycloak_scanner.scanners.scan_results import ScanResults
 from keycloak_scanner.scanners.types import ScannerType
+from keycloak_scanner.utils import to_camel_case
 
 T = TypeVar('T')
 
@@ -13,23 +14,23 @@ class Mediator:
         self.scan_results = ScanResults()
         super().__init__(**kwargs)
 
-    def send(self, result_type: ScannerType[T], value_list: List[T]) -> None:
+    def send(self, result_type: type, value_list: List[T]) -> None:
 
-        assert result_type.is_list_type(value_list)
+        result_type_name = to_camel_case(result_type.__name__)
 
-        self.scan_results.add(result_type.name, value_list)
+        self.scan_results.add(result_type_name, value_list)
 
-        if result_type.name in self.scanners:
-            for scanner in self.scanners[result_type.name]:
+        if result_type_name in self.scanners:
+            for scanner in self.scanners[result_type_name]:
                 for value in value_list:
                     scanner.receive(result_type, value)
 
     def add(self, scanner):
         scanner.set_mediator(self)
 
-    def subscribe(self, scanner, scanner_type: ScannerType):
-        if scanner_type.name in self.scanners:
-            self.scanners[scanner_type.name].append(scanner)
+    def subscribe(self, scanner, scanner_type_name: str):
+        if scanner_type_name in self.scanners:
+            self.scanners[scanner_type_name].append(scanner)
         else:
-            self.scanners[scanner_type.name] = [scanner]
+            self.scanners[scanner_type_name] = [scanner]
 
