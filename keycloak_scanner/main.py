85be72a0a7210278c10e319832ced488026f6236
neuronaddict <manufrommars@elacourarie.org>
2021-07-19 17:32:51 +0200

    Unique registration callback in args

--- keycloak_scanner/main.py
+++ keycloak_scanner/main.py
@@ -59,7 +59,6 @@ Bugs, feature requests, request another scan, questions : https://github.com/Neu
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--registration-callback', help='Callback url to use on client registration test',
-                       nargs='*',
                        default=['http://localhost'])
     group.add_argument('--registration-callback-list', help='File with one callback to test for registration by line')
 
