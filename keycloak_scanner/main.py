a1a8f4b8bc302f27e314f9230b55623abf9f19af
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2021-07-19 17:45:37 +0200

    Improve doc for clients and proxy

--- keycloak_scanner/main.py
+++ keycloak_scanner/main.py
@@ -40,10 +40,10 @@ Bugs, feature requests, request another scan, questions : https://github.com/Neu
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument('base_url', help='URL to scan. ex http://localhost:8080')
-    parser.add_argument('--realms', help='Comma separated list of custom realms to test. ie : master', default='master')
-    parser.add_argument('--clients', help='Comma separated list of custom clients to test. On default installation',
+    parser.add_argument('--realms', help='Comma separated list of custom realms to test. Default: master', default='master')
+    parser.add_argument('--clients', help='Comma separated list of custom clients to test. Default: account,admin-cli,broker,realm-management,security-admin-console',
                         default='account,admin-cli,broker,realm-management,security-admin-console')
-    parser.add_argument('--proxy', help='Use a great proxy like BURP ;)')
+    parser.add_argument('--proxy', help='Use an http proxy. ex: http://127.0.0.1:8080')
     parser.add_argument('--username', help='If a username is specified, try to connect and attack a token. If no '
                                            'password, try username as password.')
     parser.add_argument('--password', help='password to test with username')
